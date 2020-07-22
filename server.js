c5bede4ddf4c28c2458275b75d1eab8e2929e5bd
karan <kuldeepsinghsolani.111@gmail.com>
2020-07-22 11:46:18 +0530

    make it better

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
-const port = 3000;
+const bodyParser = require("body-parser");
+const api = require("./server/routes/api");
 const app = express();
-const api = require("./server/Routes/api");
-app.use(express.static(path.join(__dirname, "dist")));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "dist")));
 app.use("/api", api);
-app.get("*", (req, res) => {
+app.use("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist/index.html"));
 });
-app.listen(port, function () {
-  console.log("server running on port" + port);
+app.listen(process.env.PORT || "3000", function () {
+  console.log("server is running on port");
 });
